caf2ab9ed8a0ef08fe4bcd2cc85ae8e9a50354ab
yajamon <yajamon.tatsuki@gmail.com>
2014-07-08 19:45:37 +0900

    Add logic to "add task"

--- js/pages/index.js
+++ js/pages/index.js
@@ -4,4 +4,11 @@ taskList.addEmptyTask();
 $(function() {
 	var taskListDom = $("#taskList");
 	taskList.appendTasks(taskListDom);
+
+	// イベント登録
+	$('#tasks').on('click', '.add', function(){
+		taskList.addEmptyTask();
+		taskListDom.empty();
+		taskList.appendTasks(taskListDom);
+	});
 });
