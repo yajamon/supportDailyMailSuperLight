88744bdbaa279a22a5c9d54dac3dce68c7f72b26
yajamon <yajamon.tatsuki@gmail.com>
2014-07-10 23:29:01 +0900

    #1 reportDateの初期化処理を変更

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,24 +1,11 @@
-
-var today = new Date();
-var todayString = today.getFullYear()+'-'+zeroPadding(today.getMonth()+1, 2)+'-'+zeroPadding(today.getDate());
-
-function zeroPadding (string, length) {
-	var zeros = '';
-	for (var count = 0; count < length; count++) {
-		zeros += '0';
-	}
-	var result = zeros + string;
-	return result.substr(-length);
-}
-
 $(function() {
 	var taskListDom = $("#taskList");
 	var taskList = new TaskList(taskListDom);
+	var reportDate = new ReportDate($("#reportDate"));
+
+	reportDate.setToday();
 
 	taskList.addEmptyTask();
-
-
-	$('#reportDate').val(todayString);
 
 	// イベント登録
 	$('#tasks').on('click', '.add', function(){
