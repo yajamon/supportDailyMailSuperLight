957fb61aea9914ecb922f28b51fd147629fd599b
yajamon <yajamon.tatsuki@gmail.com>
2014-07-10 09:56:04 +0900

    報告日初期設定

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,10 +1,24 @@
 
+var today = new Date();
+var todayString = today.getFullYear()+'-'+zeroPadding(today.getMonth()+1, 2)+'-'+zeroPadding(today.getDate());
+
+function zeroPadding (string, length) {
+	var zeros = '';
+	for (var count = 0; count < length; count++) {
+		zeros += '0';
+	}
+	var result = zeros + string;
+	return result.substr(-length);
+}
 
 $(function() {
 	var taskListDom = $("#taskList");
 	var taskList = new TaskList(taskListDom);
 
 	taskList.addEmptyTask();
+
+
+	$('#reportDate').val(todayString);
 
 	// イベント登録
 	$('#tasks').on('click', '.add', function(){
