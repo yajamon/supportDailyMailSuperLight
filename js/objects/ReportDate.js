ef3a5a56bff12e4796adee6a38f13791608dd868
yajamon <yajamon.tatsuki@gmail.com>
2014-07-10 23:24:12 +0900

    #1 Add method to setting by date

--- js/objects/ReportDate.js
+++ js/objects/ReportDate.js
@@ -1,3 +1,23 @@
 function ReportDate ($obj) {
 	this.element = $obj;
 }
+
+ReportDate.prototype.setByDate = function(dateObject) {
+	var year = dateObject.getFullYear();
+	var month = dateObject.getMonth() + 1;
+	var date = dateObject.getDate();
+
+	if (month < 10) {
+		month = '0' + month;
+	}
+	if (date < 10) {
+		date = '0' + date;
+	}
+
+	var dateString = year+'-'+month+'-'+date;
+	this.element.val(dateString);
+};
+
+ReportDate.prototype.setToday = function() {
+	this.setByDate(new Date());
+};
