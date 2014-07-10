74763dd8821c80aafa70d90a9e46bfe4dbabc18c
yajamon <yajamon.tatsuki@gmail.com>
2014-07-10 10:34:44 +0900

    作業報告のメール内容を出力するようにした

--- js/pages/index.js
+++ js/pages/index.js
@@ -44,5 +44,19 @@ $(function() {
 		taskList.draw(workScheduleBody, '予定工数：');
 		workScheduleBody.put('以上、よろしくお願いいたします。');
 
+		var workReportSubject = new MailSubject($('#workReport').find('.mailSubject'));
+		var workReportBody = new MailBody($('#workReport').find('.mailBody'));
+
+		workReportSubject.clear();
+		workReportSubject.put(reportDateStr+' 作業予定');
+
+		workReportBody.clear();
+		workReportBody.put('お疲れ様です。\n');
+		workReportBody.put('\n');
+		workReportBody.put('本日の作業内容をご報告いたします。\n');
+		workReportBody.put('\n');
+		workReportBody.put('【作業報告】\n');
+		taskList.draw(workReportBody, '予定工数：');
+		workReportBody.put('以上、よろしくお願いいたします。');
 	});
 });
