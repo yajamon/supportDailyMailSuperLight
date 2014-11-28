996765caf8c17d8dfc3430a87adfd6ba19f0551c
yajamon <yajamon.tatsuki@gmail.com>
2014-11-28 11:29:46 +0900

    Fix indent

--- js/objects/ManHour.js
+++ js/objects/ManHour.js
@@ -1,23 +1,23 @@
 function ManHour (value) {
-	this.element = $('<input type="time">');
-	this.element.addClass('manHour');
-	this.element.val(value);
+    this.element = $('<input type="time">');
+    this.element.addClass('manHour');
+    this.element.val(value);
 }
 
 ManHour.prototype.append = function($obj) {
-	$obj.append(this.element);
+    $obj.append(this.element);
 };
 
 ManHour.prototype.draw = function(out) {
-	var timeString = this.element.val();
-	if (timeString == "") {
-		timeString = "00:00";
-	}
+    var timeString = this.element.val();
+    if (timeString == "") {
+        timeString = "00:00";
+    }
 
-	var splitedArray = timeString.split(':');
-	var hour = parseInt(splitedArray[0], 10);
-	var minutes = parseInt(splitedArray[1], 10);
+    var splitedArray = timeString.split(':');
+    var hour = parseInt(splitedArray[0], 10);
+    var minutes = parseInt(splitedArray[1], 10);
 
-	hour = hour + (minutes/60);
-	out.put(hour.toFixed(1)+'h\n');
+    hour = hour + (minutes/60);
+    out.put(hour.toFixed(1)+'h\n');
 };
