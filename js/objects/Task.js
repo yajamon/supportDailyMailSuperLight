79b302eedf072da9406e4bdb64675510b5ff7181
yajamon <yajamon.tatsuki@gmail.com>
2014-11-28 11:34:22 +0900

    Fix indent

--- js/objects/Task.js
+++ js/objects/Task.js
@@ -1,32 +1,32 @@
 function Task (subject, manHour, content) {
-	this.subject = subject;
-	this.manHour = manHour;
-	this.content = content;
+    this.subject = subject;
+    this.manHour = manHour;
+    this.content = content;
 }
 
 Task.prototype.append = function($obj) {
-	var task = $('<div>');
-	task.addClass('task');
+    var task = $('<div>');
+    task.addClass('task');
 
-	this.subject.append(task);
-	this.manHour.append(task);
-	this.content.append(task);
+    this.subject.append(task);
+    this.manHour.append(task);
+    this.content.append(task);
 
-	var deleteButton = $('<input type="button">');
-	deleteButton.addClass('delete');
-	deleteButton.val('タスク削除');
-	task.append(deleteButton);
+    var deleteButton = $('<input type="button">');
+    deleteButton.addClass('delete');
+    deleteButton.val('タスク削除');
+    task.append(deleteButton);
 
-	$obj.append(task);
+    $obj.append(task);
 };
 
 Task.prototype.draw = function(out, manHourPrefix) {
-	if(manHourPrefix == null){
-		manHourPrefix = '';
-	}
-	this.subject.draw(out);
-	out.put(manHourPrefix);
-	this.manHour.draw(out);
-	this.content.draw(out);
-	out.put('\n');
+    if(manHourPrefix == null){
+        manHourPrefix = '';
+    }
+    this.subject.draw(out);
+    out.put(manHourPrefix);
+    this.manHour.draw(out);
+    this.content.draw(out);
+    out.put('\n');
 };
